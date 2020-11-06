a604a6096d7b8790ed3e0e11f72ff934a8256abb
Zidni Imani <zidni.imani@gmail.com>
2020-11-06 09:23:27 +0700

    delete api

--- api/contoh_api_client/read-one.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $url = 'http://localhost:8081/evoting/api/pemilih/read_one.php?nim='.$_POST['nim'];
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $data = json_decode($response, true);
-?>
